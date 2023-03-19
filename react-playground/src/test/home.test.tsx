27565ed25da95d0a856a93e714b40477c4aab3e8
OKitel <kitelolga@gmail.com>
2023-03-19 23:54:30 +0100

    feat: add tests for cards list

--- react-playground/src/test/home.test.tsx
+++ react-playground/src/test/home.test.tsx
@@ -8,4 +8,14 @@ describe('Home test', () => {
 
     expect(screen.getByPlaceholderText(/Type here.../i)).toBeDefined();
   });
+
+  test('renders cardsField with correct number of cards', () => {
+    const { container } = render(<Home />);
+    const cardsField = container.querySelector('.cardsField');
+    expect(cardsField).toBeDefined();
+    if (cardsField) {
+      const cards = cardsField.querySelectorAll('.cardContainer');
+      expect(cards.length).toEqual(8);
+    }
+  });
 });
