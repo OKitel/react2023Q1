3be608d00cea012ac0574f0079301ec768d7ba8d
OKitel <kitelolga@gmail.com>
2023-03-19 23:17:31 +0100

    feat: change styles for 404 page

--- react-playground/src/components/NotFound/NotFound.tsx
+++ react-playground/src/components/NotFound/NotFound.tsx
@@ -3,6 +3,11 @@ import './style.css';
 
 export class NotFound extends React.Component {
   render() {
-    return <h1>404</h1>;
+    return (
+      <div className="not-found">
+        <h1 className="not-found-title">404</h1>
+        <h2>Sorry! The page you are looking for could not be found...</h2>
+      </div>
+    );
   }
 }
