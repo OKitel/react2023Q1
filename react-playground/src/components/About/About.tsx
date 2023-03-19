d3a7f8ac361a4482ee3d13a9594ba1d17d6d7900
OKitel <kitelolga@gmail.com>
2023-03-19 23:05:52 +0100

    feat: change about us page styles, add github link

--- react-playground/src/components/About/About.tsx
+++ react-playground/src/components/About/About.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './style.css';
+import GitHubImage from '../../assets/github-1.svg';
 
 export class About extends React.Component {
   render() {
-    return <h1>About</h1>;
+    return (
+      <div className="about-container">
+        <h1>About</h1>
+        <p>Looking for more? Check this out</p>
+        <a className="about-link" href="https://github.com/OKitel" target="_blank" rel="noreferrer">
+          GitHub <img src={GitHubImage} alt="GitHub" />
+        </a>
+      </div>
+    );
   }
 }
