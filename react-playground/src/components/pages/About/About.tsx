a6a0f71a8c05a0f18bbdf3d32a2acde62b83666f
OKitel <kitelolga@gmail.com>
2023-03-26 18:00:36 +0200

    refactor: extract header to app level

--- react-playground/src/components/pages/About/About.tsx
+++ react-playground/src/components/pages/About/About.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './style.css';
 import GitHubImage from '../../../assets/github-1.svg';
-import { Header } from '../../Header/Header';
 
 export class About extends React.Component {
   render() {
     return (
       <>
-        <Header title="About us" />
         <div className="about-container">
           <h1>About</h1>
           <p>Looking for more? Check this out</p>
