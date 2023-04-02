debc5c8e5a89ab76504571991a0b19771f0b7d6b
OKitel <kitelolga@gmail.com>
2023-04-02 19:19:02 +0200

    refactor: convert Gender into functional component

--- react-playground/src/components/Form/FormComponents/Gender.tsx
+++ react-playground/src/components/Form/FormComponents/Gender.tsx
@@ -6,24 +6,18 @@ type Props = {
   refTwo: React.Ref<HTMLInputElement>;
 };
 
-export class Gender extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
-  }
+export const Gender: React.FC<Props> = (props: Props) => {
+  const { refOne, refTwo } = props;
 
-  render() {
-    const { refOne, refTwo } = this.props;
-
-    return (
-      <div className="formControl">
-        Choose your gender
-        <div className="switch-field">
-          <input type="radio" id="radio-one" name="switch-one" value="male" ref={refOne} />
-          <label htmlFor="radio-one">Male</label>
-          <input type="radio" id="radio-two" name="switch-one" value="female" ref={refTwo} />
-          <label htmlFor="radio-two">Female</label>
-        </div>
+  return (
+    <div className="formControl">
+      Choose your gender
+      <div className="switch-field">
+        <input type="radio" id="radio-one" name="switch-one" value="male" ref={refOne} />
+        <label htmlFor="radio-one">Male</label>
+        <input type="radio" id="radio-two" name="switch-one" value="female" ref={refTwo} />
+        <label htmlFor="radio-two">Female</label>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
