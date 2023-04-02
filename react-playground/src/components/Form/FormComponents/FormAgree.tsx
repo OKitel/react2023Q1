f4bc21ac4e25ccf387dfba5647bb17725d2e9406
OKitel <kitelolga@gmail.com>
2023-04-02 19:17:05 +0200

    refactor: convert FormAgree into functional component

--- react-playground/src/components/Form/FormComponents/FormAgree.tsx
+++ react-playground/src/components/Form/FormComponents/FormAgree.tsx
@@ -5,22 +5,16 @@ type Props = {
   refOne: React.Ref<HTMLInputElement>;
 };
 
-export class FormAgree extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
-  }
+export const FormAgree: React.FC<Props> = (props: Props) => {
+  const { refOne } = props;
 
-  render() {
-    const { refOne } = this.props;
-
-    return (
-      <div className="formControl">
-        <label htmlFor="agree" className="agreeLabel">
-          I agree to the processing of personal data
-          <input className="hiddenCheckbox" type="checkbox" id="agree" ref={refOne}></input>
-          <span className="styledCheckbox"></span>
-        </label>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="formControl">
+      <label htmlFor="agree" className="agreeLabel">
+        I agree to the processing of personal data
+        <input className="hiddenCheckbox" type="checkbox" id="agree" ref={refOne}></input>
+        <span className="styledCheckbox"></span>
+      </label>
+    </div>
+  );
+};
