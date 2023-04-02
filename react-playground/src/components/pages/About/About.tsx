2aeecb7742ad737fb8b879c11e3aba0c65c73774
OKitel <kitelolga@gmail.com>
2023-04-02 18:49:20 +0200

    refactor: convert About into functional component

--- react-playground/src/components/pages/About/About.tsx
+++ react-playground/src/components/pages/About/About.tsx
@@ -2,23 +2,16 @@ import React from 'react';
 import './style.css';
 import GitHubImage from '../../../assets/github-1.svg';
 
-export class About extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="about-container">
-          <h1>About</h1>
-          <p>Looking for more? Check this out</p>
-          <a
-            className="about-link"
-            href="https://github.com/OKitel"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub <img src={GitHubImage} alt="GitHub" />
-          </a>
-        </div>
-      </>
-    );
-  }
-}
+export const About: React.FC = () => {
+  return (
+    <>
+      <div className="about-container">
+        <h1>About</h1>
+        <p>Looking for more? Check this out</p>
+        <a className="about-link" href="https://github.com/OKitel" target="_blank" rel="noreferrer">
+          GitHub <img src={GitHubImage} alt="GitHub" />
+        </a>
+      </div>
+    </>
+  );
+};
