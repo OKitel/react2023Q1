8f7eae3befca18eb0fa94980fdd21b7b4ec8b5e4
OKitel <kitelolga@gmail.com>
2023-04-30 21:36:44 +0200

    feat: add e2e test for form validation and form submit

--- react-playground/cypress/e2e/spec.cy.ts
+++ react-playground/cypress/e2e/spec.cy.ts
@@ -11,7 +11,7 @@ describe('e2e tests for react playground', () => {
   });
 
   it('Visits 404 page', () => {
-    cy.visit(`${URL}/abra-kadabra`);
+    cy.visit(`${URL}abra-kadabra`);
     cy.contains('404');
     cy.contains('Sorry! The page you are looking for could not be found...');
   });
@@ -35,5 +35,34 @@ describe('e2e tests for react playground', () => {
     cy.visit(URL);
     cy.get('[alt="white and black siberian husky"]').click();
     cy.contains('Linda Kazares');
+  });
+
+  it('Check form validation', () => {
+    cy.visit(`${URL}form`);
+    cy.get('[name="firstName"]').type('John');
+    cy.get('[name="lastName"]').type('Doe');
+    cy.get('[role="form"]').submit();
+    cy.contains('Birth date is required');
+    cy.contains('Country is required');
+    cy.contains('Gender is required');
+    cy.contains('Image is required');
+    cy.contains('Agree with privacy policy');
+  });
+
+  it('Create card on form submit', () => {
+    cy.visit(`${URL}form`);
+    cy.get('[name="firstName"]').type('John');
+    cy.get('[name="lastName"]').type('Doe');
+    cy.get('[name="birthDate"]').type('1978-03-21');
+    cy.get('[name="country"]').select('Poland');
+    cy.get('[for="radio-one"]').click();
+    cy.get('input[type=file]').selectFile('cypress/fixtures/deer.png', { force: true });
+    cy.get('[name="agree"]').click();
+    cy.get('[role="form"]').submit();
+    cy.contains('Name: John');
+    cy.contains('Surname: Doe');
+    cy.contains('Birthdate: 1978-03-21');
+    cy.contains('Country: Poland');
+    cy.contains('Gender: male');
   });
 });
